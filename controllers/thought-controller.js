a5b53332eb0f56f4285353f9c1db5a5e5933d547
tiannaj23 <tiannaj23@gmail.com>
2023-05-01 17:59:20 -0400

    created thought and user controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,22 @@
+const { Thought, User } = require('../models');
+const thoughtController = {
+   // get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .sort({ createdAt: -1 })
+      .then((dbThoughtData) => {
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }, 
+
+
+}
+
+
+
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,30 @@
+const {User, Thought} = require ('../models')
+const userController = {
+    getUsers(req, res) {
+        User.find()
+          .select('-__v')
+          .then((dbUserData) => {
+            res.json(dbUserData);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      },
+      createUser(req, res) {
+        User.create(req.body)
+          .then((dbUserData) => {
+            res.json(dbUserData);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      },
+}
+
+
+
+
+
+module.exports = userController;
