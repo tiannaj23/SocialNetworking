6e87c0933e6639323e11cae4e83b48abe89c92f1
tiannaj23 <tiannaj23@gmail.com>
2023-05-03 17:48:18 -0400

    added createThought and getSingleThought function

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -12,6 +12,52 @@ const thoughtController = {
         res.status(500).json(err);
       });
   }, 
+  // create a thought
+  createThought(req, res) {
+    Thought.create(req.body)
+    .then((dbThoughtData) => {
+      return User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $push: { thoughts: dbThoughtData._id } },
+        { new: true }
+      );
+    })
+    .then((dbUserData) => {
+      if(!dbUserData) {
+        return res.status(404).json({ message: "Thought has been created but no user with this id!" 
+      });
+    }
+    res.json({
+      message: "Thought created!"});
+    })
+    .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+    });
+  },
+
+  // get a single thought
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+    .then((dbThoughtData) => {
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: "User not found." });
+      }
+      res.json(dbThoughtData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  },
+  
+  // update a thought
+  updateThought(req, res) {
+    Thought.findOneAndUpdate()
+  }
+  // delete a thought
+  // add a reaction
+  // delete a reaction
 
 
 }
